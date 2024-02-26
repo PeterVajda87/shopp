16cd269f8a0395e957fd4768609028322a8abd55
Peter Vajda <p.vajda@showdowndisplays.eu>
2024-02-26 09:10:10 +0100

    Added essential pages, implemented templates

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
+mod templates;
 use ntex::server::Server;
-use ntex::web;
+use ntex::web::{self, HttpRequest};
 use std::net::TcpListener;
+use templates::index as IndexTemplate;
+use uuid::Uuid;
 
 #[web::get("/health_check")]
 async fn health_check() -> impl web::Responder {
     web::HttpResponse::Ok()
 }
 
+#[web::get("/")]
+async fn index(req: HttpRequest) -> web::HttpResponse {
+    println!("Request: {:?}", req);
+    web::HttpResponse::Ok().body(IndexTemplate::IndexPage { title: "Homepage" }.to_string())
+}
+
+#[web::get("/{slug}")]
+async fn route_by_slug(req: HttpRequest, slug: web::types::Path<String>) -> impl web::Responder {
+    println!("Request: {:?}", req);
+    format!("Hello: {}!", slug)
+}
+
+#[web::get("/product/{id}")]
+async fn product_page(req: HttpRequest, id: web::types::Path<Uuid>) -> web::HttpResponse {
+    println!("Request: {:?}", req);
+    web::HttpResponse::Ok().body(
+        IndexTemplate::IndexPage {
+            title: &format!("Product {} page", id),
+        }
+        .to_string(),
+    )
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(health_check);
+    cfg.service(health_check)
+        .service(index)
+        .service(route_by_slug)
+        .service(product_page);
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 
 #[ntex::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener: TcpListener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a port");
+    let listener: TcpListener =
+        TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to a port");
     run(listener)?.await
 }
