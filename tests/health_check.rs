f182ea8c1441a00bcdbf067984e3a1e1a58429ff
Peter Vajda <p.vajda@showdowndisplays.eu>
2024-04-04 20:10:06 +0200

    Adjusted the test to reflect refactoring done

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,10 @@
 use curl::easy::Easy;
 use ntex::{web, web::test};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use shopp::{config, run, settings::Settings};
+use shopp::{
+    config,
+    settings::{RunMode, Settings},
+    Run,
+};
 use sqlx::{postgres::PgPoolOptions, Connection, Executor, PgConnection, Pool, Postgres};
 
 pub struct TestApp {
@@ -32,40 +35,17 @@ async fn spawn_server_works() {
 }
 
 async fn spawn_server() -> TestApp {
+    let run_mode = RunMode::get();
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{port}");
 
-    let mut settings = Settings::new().expect("Failed to parse settings.");
+    let mut settings = Settings::new(&run_mode).expect("Failed to parse settings.");
     settings.database.database_name = format!("test_{}", uuid::Uuid::new_v4().to_string());
 
     let db_pool = configure_database(&settings).await;
 
-    // Load TLS keys
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    // Load the private key
-    builder
-        .set_private_key_file("settings/private.key", SslFiletype::PEM)
-        .unwrap();
-    // Load the certificate
-    builder
-        .set_certificate_chain_file("settings/certificate.crt")
-        .unwrap();
-    // Load the CA bundle, if available
-    builder
-        .set_ca_file("settings/ca_bundle.crt")
-        .unwrap_or_else(|_| {
-            println!("CA bundle not found or could not be loaded.");
-        });
-
-    let server =
-        run(db_pool.clone(), builder, settings.application_port).expect("Failed to start server.");
-
-    sqlx::migrate!()
-        .run(&db_pool)
-        .await
-        .expect("Failed to do migrations");
-
+    let server = listener.run(db_pool.clone(), settings).unwrap();
     let _ = async_std::task::spawn(server);
 
     TestApp { address, db_pool }
